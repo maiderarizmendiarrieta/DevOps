5f21fe7ac5b0ef60638c497d97ccc687c45d40d5
maiderarizmendiarrieta <arizmendiarrieta.maider@uni.eus>
2025-09-24 10:31:50 +0200

    Actionserako proiektua

---
+++ ActionserakoProiektua/src/main.py
@@ -0,0 +1,5 @@
+import os
+
+class Kalkulagailua:
+    def batu(self, a: int, b: int) -> int:
+        return a + b
---
+++ ActionserakoProiektua/src/test.py
@@ -0,0 +1,4 @@
+from main import Kalkulagailua
+
+def test_batu2_zenbaki():
+    assert Kalkulagailua().batu(2, 3) == 5
