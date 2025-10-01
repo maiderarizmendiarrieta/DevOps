cef32f173866d919f943c5e662b5bb51f084db79
maiderarizmendiarrieta <arizmendiarrieta.maider@uni.eus>
2025-10-01 09:41:15 +0200

    docker proba

--- ActionserakoProiektua/src/main.py
+++ ActionserakoProiektua/src/main.py
@@ -1,5 +1,13 @@
 import os
 
+def main():
+    print("=" * 50)
+    print("GitHub Actions + Docker martxan da!")
+    print("=" * 50)
+
 class Kalkulagailua:
     def batu(self, a: int, b: int) -> int:
         return a + b
+
+if __name__ == "__main__":
+    main()
